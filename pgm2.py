114de86e553d24fec8eb806fa82d3a82537e4af9
Bhoomiii <bhoomikapatilklb@gmail.com>
2026-02-12 23:19:07 +0530

    Update pgm2.py

--- pgm2.py
+++ pgm2.py
@@ -1,2 +1,8 @@
-import pandas
-print("hi")
+class Solution(object):
+    def runningSum(self, nums):
+        result = []
+        total = 0
+        for num in nums:
+            total += num
+            result.append(total)
+        return result
