de0ce695cb84b2ff533ec6cc6bcafd92fe307532
Bhoomiii <bhoomikapatilklb@gmail.com>
2026-02-12 23:25:13 +0530

    Update pgm2.py

--- pgm2.py
+++ pgm2.py
@@ -1,8 +1,1 @@
-class Solution(object):
-    def runningSum(self, nums):
-        result = []
-        total = 0
-        for num in nums:
-            total += num
-            result.append(total)
-        return result
+
